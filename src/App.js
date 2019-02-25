2ccaf6189cf85987f662dabb377d05471e0e2c93
Bechar Gherbi <cn.b@outlook.com>
2019-02-25 19:46:32 +0100

    Handle SearchInput Events in App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import SearchInput from "./components/SearchInput";
 class App extends Component {
+  state = {
+    searchValue: "",
+    isLoading: false
+  };
+  handleInput = value => {
+    this.setState({
+      searchValue: value
+    });
+  };
+  handleSearch = e => {
+    if (e.keyCode === 13) {
+      if (this.state.searchValue.length >= 3) {
+        alert(this.state.searchValue);
+      } else {
+        console.log(e);
+      }
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
         <Header>
-          <SearchInput />
+          <SearchInput
+            handler={this.handleInput}
+            keyUpHandler={this.handleSearch}
+            {...this.state}
+          />
         </Header>
         <Main />
         <Footer />
