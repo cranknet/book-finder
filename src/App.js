8fd982b76ecdd03ad30b1a16d2cebbf1669a92f8
Bechar Gherbi <cn.b@outlook.com>
2019-02-25 14:20:35 +0100

    Add Footer Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
         <Main />
+        <Footer />
       </React.Fragment>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+      {/* Footer */}
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Book Finder</strong> by 
+            <a href="https:github.com/cranknet"> Bechar</a>
+          </p>
+        </div>
+      </footer>
+      {/* End Footer */}
+    </React.Fragment>
+  );
+};
+
+export default Footer;
