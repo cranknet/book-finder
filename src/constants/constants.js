a045ea467e24fb6e79feea084e5ffaee289907f3
Bechar Gherbi <cn.b@outlook.com>
2019-02-25 23:13:21 +0100

    Add Constants

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:";
+export const API_URL_FIELDS =
+  "items(volumeInfo(title, authors, publisher, publishedDate, description,  imageLinks/thumbnail, infoLink))";
