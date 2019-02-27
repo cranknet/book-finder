0ba834150267d78084d3fbfebd38cb9d37eeade7
Bechar Gherbi <cn.b@outlook.com>
2019-02-27 01:45:40 +0100

    Updates

---
+++ precache-manifest.15c19942fcc382dc21f1df940323b557.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/book-finder/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "1c36b04caeb3fd49abce",
+    "url": "/book-finder/static/css/main.b2d072e9.chunk.css"
+  },
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/book-finder/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d7402598f0cd1e10dc7e",
+    "url": "/book-finder/static/js/2.d7402598.chunk.js"
+  },
+  {
+    "revision": "1c36b04caeb3fd49abce",
+    "url": "/book-finder/static/js/main.1c36b04c.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/book-finder/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/book-finder/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/book-finder/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "d7402598f0cd1e10dc7e",
+    "url": "/book-finder/static/css/2.1dbca24c.chunk.css"
+  },
+  {
+    "revision": "602607652fa008e0c19c8f10cea121c5",
+    "url": "/book-finder/index.html"
+  }
+];
