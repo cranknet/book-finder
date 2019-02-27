95f5e37d626ef72907b768a9eeed24d4db528bd6
Bechar Gherbi <cn.b@outlook.com>
2019-02-27 01:01:28 +0100

    Remove BookCard Template

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,46 +1,25 @@
 import React from "react";
-
-const Main = () => {
+import Spinner from "./Spinner";
+import BookCard from "./BookCard";
+const Main = props => {
+  const Loading = props.isLoading ? <Spinner /> : undefined;
+  const isDataLoaded =
+    props.isDataLoaded && props.data.totalItems > 0 ? (
+      <BookCard data={props.data} />
+    ) : (
+      <div className="column">
+        <h3 className="is-size-5 has-text-danger has-text-centered has-text-weight-bold">
+          No Books, Try to search!
+        </h3>
+      </div>
+    );
   return (
     <React.Fragment>
       {/* Books Cards */}
       <section className="section">
         <div className="container">
-          <div className="columns is-mobile is-multiline">
-            <div className="column is-one-quarter-desktop is-half-tablet is-half-mobile">
-              <div className="card">
-                <div className="card-image">
-                  <figure className="image is-square">
-                    <img src="content.jpeg" alt="Book" />
-                  </figure>
-                </div>
-                <div className="card-content">
-                  <div className="media">
-                    <div className="media-left">
-                      <figure className="image is-48x48">
-                        <img
-                          src="https://bulma.io/images/placeholders/96x96.png"
-                          alt="Book"
-                        />
-                      </figure>
-                    </div>
-                    <div className="media-content">
-                      <p className="title is-4">John Smith</p>
-                      <p className="subtitle is-6">@johnsmith</p>
-                    </div>
-                  </div>
-                  <div className="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                    <a href="#">#css</a>
-                    <a href="#">#responsive</a>
-                    <br />
-                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          {Loading}
+          <div className="columns is-mobile is-multiline">{isDataLoaded}</div>
         </div>
       </section>
       {/* End Books Cards */}
